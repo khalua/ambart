d2767a544a1bedcd5813d6d60e9da8fcefc06df7
Tony Contreras <tony.contreras@gmail.com>
2017-06-29 22:47:17 -0700

    added raw data for debugging in the AM

--- main.rb
+++ main.rb
@@ -7,13 +7,16 @@ require 'nokogiri'
 
 
 get '/' do
-  data = HTTParty.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ashb&key=ZSZR-595W-9QWT-DWE9&dir=s").parsed_response
+  @data = HTTParty.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ashb&key=ZSZR-595W-9QWT-DWE9&dir=s").parsed_response
 
-  @apitime = data['root']['time']
-  @destination = data['root']['station']['etd']['destination']
-  @train1 = data['root']['station']['etd']['estimate'][0]['minutes']
-  @train2 = data['root']['station']['etd']['estimate'][1]['minutes']
-  @train3 = data['root']['station']['etd']['estimate'][2]['minutes']
+  @apitime = @data['root']['time']
+  @destination = @data['root']['station']['etd']['destination']
+  @train1 = @data['root']['station']['etd']['estimate'][0]['minutes']
+  @train2 = @data['root']['station']['etd']['estimate'][1]['minutes']
+  @train3 = @data['root']['station']['etd']['estimate'][2]['minutes']
+
+
+
 
 
   erb :home
