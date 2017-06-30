9ab58055960786a0fdd750a38874a258b246ea1c
Tony Contreras <tony.contreras@gmail.com>
2017-06-29 22:43:30 -0700

    works from Ashby with just 1 desination, and looks kinda pretty

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AMbart
 
-Get me to work on time and with little wait, ya?
+Get me to work on time with a short wait, ya?
+
+Nothing fancy, just hitting this API, and doing something interesting using Sinatra
+
+http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ashb&key=ZSZR-595W-9QWT-DWE9&dir=s
---
+++ main.rb
@@ -0,0 +1,21 @@
+# This is where the magic happens
+
+require 'pry'
+require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+
+
+get '/' do
+  data = HTTParty.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ashb&key=ZSZR-595W-9QWT-DWE9&dir=s").parsed_response
+
+  @apitime = data['root']['time']
+  @destination = data['root']['station']['etd']['destination']
+  @train1 = data['root']['station']['etd']['estimate'][0]['minutes']
+  @train2 = data['root']['station']['etd']['estimate'][1]['minutes']
+  @train3 = data['root']['station']['etd']['estimate'][2]['minutes']
+
+
+  erb :home
+
+end
